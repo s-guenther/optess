f8c19389c00c26fc95dfda1f433a6165f1bb67c5
Sebastian Guenther <sebastian.guenther@ifes.uni-hannover.de>
2018-01-23 14:15:47 +0100

    [dev] * finished hybridisation opt, hardly working
    On branch develop
    Changes to be committed:
          modified:   opt_pyomo.py

--- opt_pyomo.py
+++ opt_pyomo.py
@@ -3,8 +3,12 @@
 Optimization with pyomo library.
 """
 
+from datetime import datetime
 import pyomo.environ as pe
 import itertools
+import numpy as np
+import scipy.io as scio
+import matplotlib.pyplot as plt
 
 
 def cumsum(it):
@@ -21,18 +25,33 @@ def energy(power, dtime=None):
     return cumsum(pwr * dtme for pwr, dtme in zip(power, dtime))
 
 
-def make_model():
+def load_data(filename):
+    raw = scio.loadmat(filename)
+    vals = [float(val) for val in raw['vals'].flatten()]
+    dtime = float(raw['dtime'].flatten())
+    dtime_iter = itertools.repeat(dtime)
+    return vals, dtime_iter
+
+
+def make_model(vals=None, dtime=None):
     """Builds model for a specific case (3x Tetris)"""
-    vals = [1, 0.5] * 3 + [-1, -0.5] * 3
+
+    if vals is None:
+        vals = [1, 0.5] * 3 + [-1, -0.5] * 3
+        # xx = np.linspace(2*np.pi/100, 2*np.pi, 100)
+        # vals = np.sin(xx) + np.sin(3*xx)
+    if dtime is None:
+        dtime = itertools.repeat(1)
     n = len(vals)
 
-    psinglecap = 1
-    esinglecap = max(energy(vals))
+    psinglecap = np.max(vals)
+    esinglecap = max(energy(vals, dtime))
 
-    cut = 0.75
+    cut = 0.3
     pbasecap = cut * psinglecap
     ppeakcap = (1 - cut) * psinglecap
-    mm = pe.ConcreteModel()
+
+    mm = pe.ConcreteModel(name='Hybridisation')
 
     mm.ind = pe.Set(initialize=range(n))
 
@@ -46,43 +65,66 @@ def make_model():
 
     mm.enrgycapequal = pe.Constraint(expr=(mm.ebasecap + mm.epeakcap ==
                                            esinglecap))
+    print('   {:%H:%M:%S}: Energycap Equal Constraint Done'.format(datetime.now()))
 
     def pwrequal_rule(model, ii):
         return model.pbase[ii] + model.ppeak[ii] == vals[ii]
 
     mm.pwrequal = pe.Constraint(mm.ind, rule=pwrequal_rule)
+    print('   {:%H:%M:%S}: Power Equal Rule Done'.format(datetime.now()))
 
     def enrgy_below_base_rule(model, ii):
-        enrgy = list(energy(model.pbase[:]))
+        enrgy = list(energy(model.pbase[:], dtime))
         return enrgy[ii] <= model.ebasecap
 
     def enrgy_below_peak_rule(model, ii):
-        enrgy = list(energy(model.ppeak[:]))
+        enrgy = list(energy(model.ppeak[:], dtime))
         return enrgy[ii] <= model.epeakcap
 
     def enrgy_base_above_zero_rule(model, ii):
-        enrgy = list(energy(model.pbase[:]))
-        return 0 <= enrgy[ii]
+        enrgy = list(energy(model.pbase[:], dtime))
+        return -0.02 <= enrgy[ii]
 
     def enrgy_peak_above_zero_rule(model, ii):
-        enrgy = list(energy(model.ppeak[:]))
+        enrgy = list(energy(model.ppeak[:], dtime))
         return 0 <= enrgy[ii]
 
+    print('   {:%H:%M:%S}: Rule Fcn Def Done'.format(datetime.now()))
+
     mm.enrgybelowbase = pe.Constraint(mm.ind, rule=enrgy_below_base_rule)
+    print('   {:%H:%M:%S}: Energy Below Base Done'.format(datetime.now()))
     mm.enrgybelowpeak = pe.Constraint(mm.ind, rule=enrgy_below_peak_rule)
+    print('   {:%H:%M:%S}: Energy Below Peak Done'.format(datetime.now()))
     mm.enrgybaseabovezero = pe.Constraint(mm.ind,
                                           rule=enrgy_base_above_zero_rule)
+    print('   {:%H:%M:%S}: Energy Base Above Zero Done'.format(datetime.now()))
     mm.enrgypeakabovezero = pe.Constraint(mm.ind,
                                           rule=enrgy_peak_above_zero_rule)
+    print('   {:%H:%M:%S}: Energy Peak Above Zero Done'.format(datetime.now()))
 
-    return mm
+    return mm, vals, n
 
 
-def main():
-    model = make_model()
-    # solver = pe.SolverFactory('glpk')
-    # solver.solve(model)
+def main(filename='rand03.mat'):
+    print('{:%H:%M:%S}: Loading Files...'.format(datetime.now()))
+    vals, dtime = load_data(filename)
+    print('{:%H:%M:%S}: Making Model...'.format(datetime.now()))
+    model, vals, n = make_model(vals, dtime)
+    print('{:%H:%M:%S}: Setting up Solver...'.format(datetime.now()))
+    solver = pe.SolverFactory('gurobi')
+    print('{:%H:%M:%S}: Solving Model...'.format(datetime.now()))
+    solver.solve(model)
     # model.pprint()
+    print('{:%H:%M:%S}: Starting Post Processing...'.format(datetime.now()))
+    xx = np.linspace(2*np.pi/n, 2*np.pi, n)
+    plt.step(xx, vals)
+    plt.step(xx, model.ppeak.get_values().values())
+    plt.step(xx, model.pbase.get_values().values())
+    ebasecap = model.ebasecap.get_values().values()
+    epeakcap = model.epeakcap.get_values().values()
+    print('\n   Base Energy Capacity = {}'.format(ebasecap))
+    print('   Peak Energy Capacity = {}'.format(epeakcap))
     return model
 
-mymodel = main()
+
+model = main()
