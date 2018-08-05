b5409ae70fac585bbb6e52edda17260ce383cdc5
Sebastian Guenther <sebastian.guenther@ifes.uni-hannover.de>
2018-08-05 14:12:56 +0200

    [dev] * corrected shell scripts resources/second*
    On branch develop
    Changes to be committed:
          modified:   scripts/resources/second*.sh

--- scripts/resources/second1500.sh
+++ scripts/resources/second1500.sh
@@ -6,12 +6,14 @@
 #PBS -N second1500
 #PBS -M sebastian.guenther@ifes.uni-hannover.de
 #PBS -m abe
+#PBS -j oe
 
 module load GCC/4.9.3-2.25 OpenMPI/1.10.2
 module load Python/3.4.3
 module load numpy/1.10.1-Python-3.4.3
 module load scipy/0.17.1-Python-3.4.3
 module load Gurobi/7.5.2
+module load matplotlib/1.5.1-Python-3.4.3
 
 cd $BIGWORK/large
 
