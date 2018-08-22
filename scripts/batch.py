64abfacbf3e72d104c279dcb8aec3b03726204f6
Sebastian Guenther <sebastian.guenther@ifes.uni-hannover.de>
2018-08-22 10:30:33 +0200

    [dev] - moved scripts to separate project
    ... to avoid clutter
    
    On branch develop
    Changes to be committed:
          modified:   .gitignore
          deleted:    scripts/abunna.py
          deleted:    scripts/abunnasmall.py
          modified:   scripts/batch.py
          deleted:    scripts/resources/*

--- scripts/batch.py
+++ scripts/batch.py
@@ -8,24 +8,6 @@ import timeit
 
 import optess as oe
 
-# storage = oe.Storage(2, 0.95, 0.01)
-# storage = oe.Storage(2.0, 0.95, 1e-2)
-# objective = oe.ObjectiveFactory.std03()
-
-# start = timeit.default_timer()
-# setup_sess = oe.SingleSetupFactory.std()
-# optim_sess = oe.OptimizeSingleESS(*setup_sess)
-# optim_sess.pplot()
-# end = timeit.default_timer() - start
-# print('SESS Calculation went for {} seconds'.format(end))
-#
-# start = timeit.default_timer()
-# setup_hess = oe.HybridSetupFactory.std()
-# optim_hess = oe.OptimizeHybridESS(*setup_hess)
-# optim_hess.pplot()
-# end = timeit.default_timer() - start
-# print('HESS Calculation went for {} seconds'.format(end))
-
 peakcut = 3
 objective = oe.Objective('power', peakcut)
 for npoints in [44, 66, 88, 110, 220, 440]:
