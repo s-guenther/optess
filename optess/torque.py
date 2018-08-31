2a7e6d3aa2f2932351b032b66e00d469007d5d86
Sebastian Guenther <sebastian.guenther@ifes.uni-hannover.de>
2018-08-31 15:10:11 +0200

    [dev] + initialize directory, unfinished
    On branch cluster
    Changes to be committed:
          modified:   optess/torque.py

--- optess/torque.py
+++ optess/torque.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 import xmltodict
 from glob import glob
 import os
+import sys
 
 
 class NoTorqueSetupFileFoundError(FileNotFoundError):
@@ -53,27 +54,65 @@ area calculation
 '''[1:-1]
 
 
+JOIN_CURVE_SH = '''
+Python file for
+area calculation
+'''[1:-1]
+
+
+JOIN_CURVE_PY = '''
+Python file for
+area calculation
+'''[1:-1]
+
+
+JOIN_AREA_SH = '''
+Python file for
+area calculation
+'''[1:-1]
+
+
+JOIN_AREA_PY = '''
+Python file for
+area calculation
+'''[1:-1]
+
+
+CLEANUP_SH = '''
+Python file for
+area calculation
+'''[1:-1]
+
+
 def qsub(file, parameters=None, pbs=None):
     jobid = None
     return jobid
 
 
 _Scale = namedtuple('_Scale', 'wt mem')
+_Resources = namedtuple('_Resources', 'points time cpu ram')
 
 
 class Torque:
     def __init__(self, hyb, setupfile=None, wt=1.0, mem=1.0):
         self.name = hyb.name
         self.npoints = len(hyb.signal)
-        self.setup = self.read_setup(setupfile)
+
         self.scale = _Scale(wt, mem)
+
+        setup = self.read_setup(setupfile)
+        self.workdir, self.modules, self.pbs, self.mail, self.resources = setup
+        self.savedir = os.path.join(self.workdir, self.name)
+        self.tmpdir = os.path.join(self.savedir, 'tmp_{}'.format(self.name))
+        self.logdir = os.path.join(self.savedir, 'log_{}'.format(self.name))
+
         self.singleid = None
         self.interids = list()
         self.nointerids = list()
         self.areaids = list()
         self.utilityids = list()
 
-        self.initialize(self, hyb)
+        self.initialize(hyb)
 
     def qsub(self, cuts, curves):
         # do all
@@ -105,16 +144,23 @@ class Torque:
 
     def initialize(self, hyb):
         # TODO change path to setuppath
-        os.mkdir(self.name)
-        tmpdir = 'tmp_{}'.format(self.name)
-        os.mkdir(tmpdir)
-        hyb.save()
+        os.mkdir(self.savedir)
+        os.mkdir(self.tmpdir)
+        os.mkdir(self.logdir)
+        hyb.save(os.path.join(self.workdir, '{}.hyb'.format(self.name)))
         filenames = ('single.sh', 'single.py', 'curve.sh', 'curve.py',
-                     'area.sh', 'area.py')
-        contents = (SINGLE_SH, SINGLE_PY, CURVE_SH, CURVE_PY, AREA_SH, AREA_PY)
+                     'area.sh', 'area.py', 'join_curve.sh', 'join_curve.py',
+                     'join_area.sh', 'join_area.py', 'cleanup.sh')
+        contents = (SINGLE_SH, SINGLE_PY, CURVE_SH, CURVE_PY, AREA_SH,
+                    AREA_PY, JOIN_CURVE_SH, JOIN_CURVE_PY, JOIN_AREA_SH,
+                    JOIN_AREA_PY, CLEANUP_SH)
         for filename, content in zip(filenames, contents):
             with open(os.path.join(tmpdir, filename), 'w') as file:
                 file.write(content)
+
+        logfilename = os.path.join(self.workdir, '{}.log'.format(self.name))
+        with open(logfilename) as logfile:
+            logfile.write('Initialized all directories\n')
 
     def status(self):
         pass
@@ -131,10 +177,25 @@ class Torque:
                 raise NoTorqueSetupFileFoundError
 
         with open(setupfile, 'wb') as file:
-            torquesetup = xmltodict.parse(file.read())
+            setup = xmltodict.parse(file.read())
 
-        torquekeys = ('workdir', 'modules', 'single', 'curve', 'area')
-        valid = all(key in torquesetup for key in torquekeys)
-        if not valid:
-            raise TorqueSetupIncompleteError
-        return setup
+        workdir = setup['workdir']
+        modules = 'module load ' + '\nmodule load '.join(setup['module'])
+        try:
+            pbs = ' '.join(setup['pbs'])
+        except 'KeyError':
+            pbs = ''
+        mail = setup['mail']
+        resources = dict()
+        for calc in ['single', 'curve', 'area']:
+            resources[calc] = \
+                _Resources(_string_to_numlist(setup[calc]['points']),
+                           _string_to_numlist(setup[calc]['time']),
+                           _string_to_numlist(setup[calc]['cpu']),
+                           _string_to_numlist(setup[calc]['ram']))
+
+        return workdir, modules, pbs, mail, resources
+
+
+def _string_to_numlist(string):
+    return [int(val) for val in string.split()]
